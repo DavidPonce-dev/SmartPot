771a63e6a3a194644ac9bfb62b8b30f23bfd8383
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-09 02:14:32 -0400

    agregando configuraciones a wifimanager

--- esp8266-client/esp8266-client.ino
+++ esp8266-client/esp8266-client.ino
@@ -1,14 +1,25 @@
 #include <WiFiManager.h>
 
-void setup() {
-    Serial.begin(115200);
+void wifiManager() {
     WiFiManager wm;
+    WiFiManagerParameter js("<script>var h1 = document.createElement(\"h1\"); h1.innerHTML = \"Configuracion wifi\"; document.querySelector(\"body\").prepend(h1);</script>");
+    wm.setCustomHeadElement("<script>if (window.location.href.indexOf(\"/wifi\")<0) {window.location=\"/wifi\"};</script>");
+    wm.addParameter(&js);
+    wm.setClass("invert");
+    wm.setConnectTimeout(20);
     if ( !wm.autoConnect("SmartPot-setup" ) ) {
         Serial.println("Error al conectar");
     }
     else {
         Serial.println("Conectado!!");
     }
+}
+
+void setup() {
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    delay(2000);
+    wifiManager();
 }
 
 void loop(){
