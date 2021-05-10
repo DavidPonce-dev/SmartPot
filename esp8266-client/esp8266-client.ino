2f5a0671420ff23f300af23f6a2fcf32cf3d8f27
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-10 02:27:46 -0400

    limpiando

--- esp8266-client/esp8266-client.ino
+++ esp8266-client/esp8266-client.ino
@@ -1,17 +1,12 @@
 #include "libs/wifiManager.cpp"
 #include "libs/SignIn.cpp"
 
-String dominio = "http://localhost:3000";
+String dominio = "http://192.168.43.168:3000";
 SignIn signIn;
 
 void setup() {
     Serial.begin(115200);
-    Serial.print("\n");
-    Serial.print("\n");
-    Serial.print("Iniciando Smartpot...\n");
-    delay(2000);
     wifiManager();
-    delay(2000);
     signIn.begin(dominio + "/login", dominio + "/register");
 }
 
