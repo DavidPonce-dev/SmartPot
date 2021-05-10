59750c58ef5829b73aeb732660194083915b8b16
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-10 01:41:56 -0400

    creando servidor express y rutas principales

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Hola mundo!!asd");
+const express = require("express");
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hola mundo!');
+});
+app.get('/login', function(req, res) {
+    res.send('pagina de login!');
+});
+app.get('/register', function(req, res) {
+    res.send('Pagina de registro!');
+});
+
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000");
+});
