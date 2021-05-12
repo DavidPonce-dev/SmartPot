b11b0e86aa1674db7b5b958d4f2b82f80a485ac5
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-12 04:04:39 -0400

    rutas api

---
+++ server/routes/potAPI.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const bodyParser = require('body-parser')
+
+router.post('/login', bodyParser.json(), function(req, res) {
+    console.log(req.body);
+});
+router.post('/register', bodyParser.json(), function(req, res) {
+    console.log(req.body);
+});
+
+module.exports = router
