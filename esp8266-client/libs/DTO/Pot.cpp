fc7bba680e66f9a553d48fa4210eb14c05473693
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-17 06:31:03 -0400

    creando getters y setters

--- esp8266-client/libs/DTO/Pot.cpp
+++ esp8266-client/libs/DTO/Pot.cpp
@@ -7,6 +7,39 @@ class Pot{
         this->waterLevel = waterLvl;
         this->density = ppm;
         this->waterPh = ph;
+        this->apikey = apikey;
+    }
+
+    String getApikey(){
+        return apikey;
+    }
+
+    int getWaterLevel(){
+        return waterLevel;
+    }
+
+    int getDensity(){
+        return density;
+    }
+
+    int getWaterPh(){
+        return waterPh;
+    }
+
+    void setApikey(String apikey){
+        this->apikey = apikey;
+    }
+
+    void setWaterLevel(int waterLevel){
+        this->waterLevel = waterLevel;
+    }
+
+    void setDensity(int density){
+        this->density = density;
+    }
+
+    void setWaterPh(int waterPh){
+        this->waterPh = waterPh;
     }
 
     private:
