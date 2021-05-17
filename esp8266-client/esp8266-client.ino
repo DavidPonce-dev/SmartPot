c4938cf2cd12b55df39b74a3c921b7c9da38bb5b
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-05-17 05:56:41 -0400

    eliminando archivo de wifimanager

--- esp8266-client/esp8266-client.ino
+++ esp8266-client/esp8266-client.ino
@@ -1,12 +1,27 @@
+#include <WiFiManager.h>
 #include "libs/wifiManager.cpp"
-#include "libs/SignIn.cpp"
+#include "libs/Pot.cpp"
 
-SignIn signIn("http://192.168.43.236:3000");
+Pot pot;
+
+const char* apSSID = "SmartPot-setup";
+
+void wifiManagerConfig() {
+    WiFi.mode(WIFI_STA);
+    WiFiManager wm;
+    WiFiManagerParameter js("<script>var h1 = document.createElement(\"h1\"); h1.innerHTML = \"Configuracion wifi\"; document.querySelector(\"body\").prepend(h1);</script>");
+    wm.setCustomHeadElement("<script>if (window.location.href.indexOf(\"/wifi\")<0) {window.location=\"/wifi\"};</script>");
+    wm.addParameter(&js);
+    wm.setClass("invert");
+    wm.setConnectTimeout(10);
+
+    if( !wm.autoConnect( apSSID ) ) ESP.restart();
+}
 
 void setup() {
     Serial.begin(115200);
-    wifiManager();
-    signIn.begin();
+    wifiManagerConfig();
+    pot.begin("http://192.168.43.236:3000");
 }
 
 void loop(){
