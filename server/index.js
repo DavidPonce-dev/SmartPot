f88cdbc58342363c6a7976a4ab437d2730209978
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-12-20 20:46:25 -0300

    optimizando llamadas a middlewares

--- server/index.js
+++ server/index.js
@@ -7,18 +7,17 @@ const potRoutes = require('./routes/potAPI')
 const webRoutes = require('./routes/web')
 
 app.use(webRoutes)
-app.use('/api/', potRoutes)
+app.use('/api/pot/', express.json(), express.urlencoded() , potRoutes)
 
 //middlewares
 
 app.use(require("morgan")("dev"));
-app.use(express.json())
 
 //listener
 
 const main = async () => {
     try {
-        await require("./db");
+        //await require("./db");
         console.log("DB iniciada");
         app.listen(3000, ()=>{
             console.log("Servidor corriendo en el puerto 3000");
--- server/routes/potAPI.js
+++ server/routes/potAPI.js
@@ -1,11 +1,10 @@
 const router = require('express').Router()
-const bodyParser = require('body-parser')
 
-router.post('/signin', bodyParser.json(), function(req, res) {
+router.post('/signin', function(req, res) {
     console.log(req.body);
 });
 
-router.post('/getdata', bodyParser.json(), function(req, res) {
+router.get('/getdata',function(req, res) {
     console.log(req.body);
 });
 
