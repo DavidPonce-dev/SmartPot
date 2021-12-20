73db3fa497a947d221cb9c83e2dcd2be77848f40
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-12-20 20:44:55 -0300

    actualizando API

--- esp8266-client/libs/Fetch.cpp
+++ esp8266-client/libs/Fetch.cpp
@@ -1,16 +1,20 @@
 #include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+
+WiFiClient wifiClient;
 
 class Fetch{
     public:
     String GET(String url, String (*callback)(int, String)){
-        http.begin(url.c_str());
+        http.begin(wifiClient, url.c_str());
         httpResponseCode = http.GET();
         payload = http.getString();
         http.end();
         return callback(httpResponseCode, payload);
     }
     String POST(String url, String data, String (*callback)(int, String)){ //(httpCode, response)
-        http.begin(url.c_str());
+        http.begin(wifiClient, url.c_str());
         http.addHeader("Content-Type", "application/json");
         httpResponseCode = http.POST(data);
         payload = http.getString();
