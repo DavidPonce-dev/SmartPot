c12a3b7eafec0657ea264c1b23b07a62fb0acc63
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-12-23 01:41:36 -0300

    ordenando estructura

--- esp8266-client/esp8266-client.ino
+++ esp8266-client/esp8266-client.ino
@@ -1,41 +1,65 @@
 #include <WiFiManager.h>
 #include "libs/PotApi.cpp"
 
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3f,16,2);
-
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
 PotApi pot;
 
-const char* apSSID = "SmartPot-setup";
-
-void wifiManagerConfig() {
-    WiFi.mode(WIFI_STA);
-    WiFiManager wm;
-    WiFiManagerParameter js("<script>var h1 = document.createElement(\"h1\"); h1.innerHTML = \"Configuracion wifi\"; document.querySelector(\"body\").prepend(h1);</script>");
-    wm.setCustomHeadElement("<script>if (window.location.href.indexOf(\"/wifi\")<0) {window.location=\"/wifi\"};</script>");
-    wm.addParameter(&js);
-    wm.setClass("invert");
-    wm.setConnectTimeout(10);
-
-    if( !wm.autoConnect( apSSID ) ) ESP.restart();
+void lcdInit() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Hola! SmartPot");
+  lcd.setCursor(0, 1);
+  lcd.print("V0.0.1");
+  delay(2000);
 }
 
-void setup() {
-    
-  lcd.init();            // our display has 16 cols, 2 rows
-  lcd.backlight();      // Turn on the backlight
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
+void lcdConfig(WiFiManager *myWiFiManager)
+{
+  lcd.setCursor(0, 0);
+  lcd.println("Wifi config mode");
+  lcd.setCursor(0, 1);
+  lcd.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void lcdConnected(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Conectado!!");
   lcd.setCursor(0,1);
-  lcd.print("Ywrobot Arduino!");
-
-    Serial.begin(115200);
-    wifiManagerConfig();
-    pot.begin("http://192.168.43.236:3000");
+  lcd.print(" a la red");
+  delay(2000);
 }
 
-void loop(){
+void wmSetup()
+{
+  WiFi.mode(WIFI_STA);
+  WiFiManager wm;
+  WiFiManagerParameter js("<script>var h1 = document.createElement(\"h1\"); h1.innerHTML = \"Configuracion wifi\"; document.querySelector(\"body\").prepend(h1);</script>");
+  wm.setCustomHeadElement("<script>if (window.location.href.indexOf(\"/wifi\")<0) {window.location=\"/wifi\"};</script>");
+  wm.addParameter(&js);
+  wm.setClass("invert");
+  wm.setConnectTimeout(10);
+  wm.setAPCallback(lcdConfig);
+
+  if (!wm.autoConnect("SmartPot-setup"))
+    ESP.restart();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  lcdInit();
+  wmSetup();
+  lcdConnected();
+  pot.begin("http://192.168.43.236:3000");
+}
+
+void loop()
+{
   pot.loop();
 }
